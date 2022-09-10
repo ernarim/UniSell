e756e7264ad1a48376cf9acfe2f5da2bda081750
mbdulkadiroglu <mbdulkadiroglu@gmail.com>
2022-09-10 20:49:01 +0300

    Favorites page bug correction

--- lib/FavouritesPage.dart
+++ lib/FavouritesPage.dart
@@ -3,9 +3,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:untitled1/Product.dart';
-import 'package:untitled1/ProductPage.dart';
 import 'package:untitled1/ProfilePage.dart';
-import 'package:untitled1/UserClass.dart';
+
+import 'ProductPage.dart';
 
 class FavouritesPage extends StatefulWidget {
   const FavouritesPage({Key? key}) : super(key: key);
@@ -14,42 +14,18 @@ class FavouritesPage extends StatefulWidget {
   State<FavouritesPage> createState() => _FavouritesPageState();
 }
 
-
 class _FavouritesPageState extends State<FavouritesPage> {
-
-  late List<String> favIdList = <String>[];
-  late List<Product> favProducts = <Product>[];
+  var favIdList = <dynamic>[];
 
   var userId = FirebaseAuth.instance.currentUser?.uid;
-  late var refUser= FirebaseFirestore.instance.collection("users").doc(userId);
+  late var refUser = FirebaseFirestore.instance.collection("users").doc(userId);
   var refProducts = FirebaseFirestore.instance.collection("products");
-
-  Future<void> getFavIdList() async{
-    final docSnap = await refUser.get();
-    final user = UserClass.fromFirestore(docSnap);
-    favIdList = user.userFavourites;
-    print(favIdList);
-  }
-
-  Future<void> getFavProducts() async{
-    await getFavIdList();
-    refProducts.get().then((value) => {
-      value.docs.forEach((element) {
-        var product = Product.fromFirestore(element);
-        if(favIdList.contains(product.productId)){
-          favProducts.add(product);
-        }
-      }),
-      print(favProducts),
-    });
-  }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -103,11 +79,8 @@ class _FavouritesPageState extends State<FavouritesPage> {
                 flex: 20,
                 child: GestureDetector(
                   onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ProfilePage(
-                            )));
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => ProfilePage()));
                   },
                   child: Container(
                     height: 45,
@@ -125,115 +98,154 @@ class _FavouritesPageState extends State<FavouritesPage> {
               )
             ],
           ),
-
-          Expanded(
-            child: FutureBuilder(
-
-              future: getFavProducts(),
-                builder: (context, AsyncSnapshot event) {
-                    return ListView.builder(
-                        itemCount: favProducts.length,
-                        padding: const EdgeInsets.fromLTRB(10, 5, 10, 40),
-                        itemBuilder: (context, indeks) {
-                          return SizedBox(
-                            height: 90,
-                            width: 350,
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.push(
+          StreamBuilder<DocumentSnapshot>(
+            stream: refUser.snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData && !snapshot.hasError) {
+                favIdList = snapshot.data?.get("user_favourites");
+                return Expanded(
+                  child: ListView.builder(
+                    itemCount: favIdList.length,
+                    itemBuilder: (context, index) {
+                      return StreamBuilder<DocumentSnapshot>(
+                        stream: refProducts
+                            .doc(favIdList[index] as String)
+                            .snapshots(),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData && !snapshot.hasError) {
+                            var product = snapshot.data!;
+                            return SizedBox(
+                              height: 90,
+                              width: 350,
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
                                     context,
                                     MaterialPageRoute(
-                                        builder: (context) => ProductPage(
-                                          product: favProducts[indeks],
-                                        )));
-                              },
-                              child: Card(
-                                semanticContainer: true,
-                                clipBehavior: Clip.antiAliasWithSaveLayer,
-                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                                color: Colors.black.withOpacity(0.15),
-                                elevation: 0,
-                                margin: const EdgeInsets.all(6),
-                                child: Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
+                                      builder: (context) => ProductPage(
+                                        product: Product(
+                                            product["product_id"],
+                                            product["product_name"],
+                                            product["product_price"],
+                                            product["product_description"],
+                                            "productPhotoPath",
+                                            "productOwnerId",
+                                            ["productCategories"],
+                                            true),
+                                      ),
+                                    ),
+                                  );
+                                },
+                                child: Card(
+                                  semanticContainer: true,
+                                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20)),
+                                  color: Colors.black.withOpacity(0.15),
+                                  elevation: 0,
+                                  margin: const EdgeInsets.all(6),
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
                                       Container(
-                                        width: 75,
-                                        height: 75,
-                                        margin: EdgeInsets.all(6),
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(20),
+                                          width: 75,
+                                          height: 75,
+                                          margin: EdgeInsets.all(6),
+                                          decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(20),
                                             color: Colors.black,
-                                            image: DecorationImage(image: const AssetImage("assets/images/basys.jpg"),
-                                                fit: BoxFit.fill,
+                                            image: DecorationImage(
+                                              image: const AssetImage(
+                                                  "assets/images/basys.jpg"),
+                                              fit: BoxFit.fill,
                                               colorFilter: ColorFilter.mode(
                                                   Colors.black
                                                       .withOpacity(0.45),
                                                   BlendMode.darken),
                                             ),
-                                        )
+                                          )),
+                                      SizedBox(width: 5),
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          SizedBox(height: 8),
+                                          Text(
+                                            product["product_name"],
+                                            style: const TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.w500),
+                                            textAlign: TextAlign.start,
+                                          ),
+                                          SizedBox(height: 3),
+                                          Text(
+                                            product["product_price"],
+                                            style: const TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.w500),
+                                            textAlign: TextAlign.start,
+                                          ),
+                                          SizedBox(height: 8),
+                                          Container(
+                                            height: 20,
+                                            padding: const EdgeInsets.fromLTRB(
+                                                15, 0, 15, 0),
+                                            decoration: BoxDecoration(
+                                                borderRadius:
+                                                    BorderRadius.circular(20),
+                                                color: Colors.black),
+                                            child: const Center(
+                                                child: Text("School",
+                                                    style: TextStyle(
+                                                        color: Colors.white,
+                                                        fontWeight:
+                                                            FontWeight.w500,
+                                                        fontSize: 10))),
+                                          ),
+                                        ],
                                       ),
-                                    SizedBox(width: 5),
-                                    Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        SizedBox(height: 8),
-                                        Text(
-                                          "${favProducts[indeks].productName}",
-                                          style: const TextStyle(
-                                              color: Colors.black,
-                                              fontSize: 14,
-                                              fontWeight: FontWeight.w500),
-                                              textAlign: TextAlign.start,
-                                        ),
-                                        SizedBox(height: 3),
-                                        Text(
-                                          "${favProducts[indeks].productPrice}\$",
-                                          style: const TextStyle(
-                                              color: Colors.black,
-                                              fontSize: 12,
-                                              fontWeight: FontWeight.w500),
-                                          textAlign: TextAlign.start,
-                                        ),
-                                        SizedBox(height: 8),
-
-
-                                        Container(
-                                          height: 20,
-                                          padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
-                                          decoration: BoxDecoration(
-                                              borderRadius: BorderRadius.circular(20),
-                                              color: Colors.black),
-                                          child: const Center(
-                                            child: Text(
-                                              "School", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 10))),
-                                        ),
-
-                                      ],
-                                    ),
-                                    Spacer(),
-                                    MaterialButton(
-                                      height: 24.0,
-                                      minWidth: 24.0,
-                                      color: Colors.black,
-                                      child: Icon(Icons.remove, color: Colors.white, size: 18),
-                                      splashColor: Colors.grey,
-                                      shape: new CircleBorder(),
-                                      onPressed: () =>{},
-                                    )
-                                  ],
+                                      Spacer(),
+                                      MaterialButton(
+                                        height: 24.0,
+                                        minWidth: 24.0,
+                                        color: Colors.black,
+                                        splashColor: Colors.grey,
+                                        shape: CircleBorder(),
+                                        onPressed: () => {},
+                                        child: Icon(Icons.remove,
+                                            color: Colors.white, size: 18),
+                                      )
+                                    ],
+                                  ),
                                 ),
-                              )
-                            ),
-                          );
-                        });
-                }),
-
+                              ),
+                            );
+                          } else {
+                            return Text(snapshot.error.toString());
+                          }
+                        },
+                      );
+                    },
+                  ),
+                );
+              } else {
+                return Container(
+                  margin: EdgeInsets.all(7.0),
+                  height: 15,
+                  child: LinearProgressIndicator(
+                    color: Colors.black45,
+                    backgroundColor: Colors.grey,
+                  ),
+                );
+              }
+            },
           )
-
-
         ],
-      )
+      ),
     );
   }
 }
